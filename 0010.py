889eb41e530342874cba78b75101ad6e38413d74
Suman-01 <sangitsuman987@gmail.com>
2026-05-28 15:51:45 +0530

    Optimized Sieve of eratosthenes

--- 0010.py
+++ 0010.py
@@ -2,16 +2,18 @@ import time
 
 n = 2000000
 s = 0
-primes = [True] * n
-primes[0] = primes[1] = False
 start = time.perf_counter()
 
-for i in range(2, int(n**0.5) + 1):
-    if primes[i]:
-        for j in range(i*i, n, i):
-            primes[j] = False
+primes = [False] * (n + 1)
+primes[2] = primes[3] = True
+primes[5 : n + 1 : 6] = [True] * len(primes[5 : n + 1 : 6])
+primes[7 : n + 1 : 6] = [True] * len(primes[7 : n + 1 : 6])
 
-s = sum(i for i, p in enumerate(primes) if p)
+for i in range(5, int(n**0.5) + 1):
+    if primes[i]:
+        primes[i * i : n : i] = [False] * len(primes[i * i : n : i])
+
+s = sum(i for i in range(n) if primes[i])
 end = time.perf_counter()
 delta = end - start
 
@@ -19,8 +21,13 @@ delta = end - start
 print("sum =", s)
 print(f"time taken: {delta:.5f}s")
 
-# OUTPUT:
+# OUTPUT (Current):
+# sum = 142913828922
+# time taken: 0.08413s
+
+# OUTPUT (Simple Sieve):
 # sum = 142913828922
 # time taken: 0.28539s
+
 
 
